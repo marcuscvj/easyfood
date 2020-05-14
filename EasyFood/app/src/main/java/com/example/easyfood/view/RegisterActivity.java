4938b22bff2163ed71083ad5ecb9b91c226a4a18
Olof Haga <oh222fv@student.lnu.se>
2020-05-14 10:00:23 +0200

    Adding emailTV, passwordTV and registerBtn

--- EasyFood/app/src/main/java/com/example/easyfood/view/RegisterActivity.java
+++ EasyFood/app/src/main/java/com/example/easyfood/view/RegisterActivity.java
@@ -1,14 +1,24 @@
 package com.example.easyfood.view;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.easyfood.R;
 
 public class RegisterActivity extends BaseActivity {
 
+    TextView emailTextView;
+    TextView passwordTextView;
+    Button registerButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailTextView = findViewById(R.id.email_textView);
+        passwordTextView = findViewById(R.id.password_textView);
+        registerButton = findViewById(R.id.register_button);
     }
 }
