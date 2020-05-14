6b58b1f15f20a35196c5d5ee6a7be096a6aa58f6
Olof Haga <oh222fv@student.lnu.se>
2020-05-14 09:49:57 +0200

    Creating register activity

---
+++ EasyFood/app/src/main/java/com/example/easyfood/view/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.easyfood.view;
+
+import android.os.Bundle;
+
+import com.example.easyfood.R;
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
