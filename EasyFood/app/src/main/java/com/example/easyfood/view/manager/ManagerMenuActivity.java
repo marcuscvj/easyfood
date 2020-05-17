f0a42f9020cd74e576c5ee77958b716dca9ad4ec
Olof Haga <oh222fv@student.lnu.se>
2020-05-17 15:04:50 +0200

    Creating Activity and Layout

---
+++ EasyFood/app/src/main/java/com/example/easyfood/view/manager/ManagerMenuActivity.java
@@ -0,0 +1,14 @@
+package com.example.easyfood.view.manager;
+
+import android.os.Bundle;
+
+import com.example.easyfood.R;
+import com.example.easyfood.view.BaseActivity;
+
+public class ManagerMenuActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_manager);
+    }
+}
