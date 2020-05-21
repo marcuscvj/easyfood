2131fbc0af311bbae8a02410063d9accd10da174
Olof Haga <oh222fv@student.lnu.se>
2020-05-21 23:12:19 +0200

    Creating functionality for SearchView

--- EasyFood/app/src/main/java/com/example/easyfood/view/EateryAdapter.java
+++ EasyFood/app/src/main/java/com/example/easyfood/view/EateryAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -18,8 +20,9 @@ import java.util.List;
 /**
  * Represents an EateryAdapter
  */
-public class EateryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class EateryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements Filterable {
     private List<Eatery> eateries;
+    private List<Eatery> eateriesFull;
     private Context context;
     private OnRestaurantListener onRestaurantListener;
 
@@ -31,6 +34,8 @@ public class EateryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
      */
     public EateryAdapter(Context context, List<Eatery> eateries, OnRestaurantListener onRestaurantListener) {
         this.eateries = eateries;
+        // this.eateriesTemp = new ArrayList<>();
+        this.eateriesFull = new ArrayList<>();
         this.context = context;
         this.onRestaurantListener = onRestaurantListener;
     }
@@ -51,6 +56,69 @@ public class EateryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     public int getItemCount() {
         return eateries.size();
     }
+
+    /**
+     * Updates the RecyclerView with a new list of eateries.
+     *
+     * @param eateries : List<Eatery> - The list of eateries
+     */
+    public void setEateries(List<Eatery> eateries) {
+        this.eateries = eateries;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Updates the backup eatery list
+     *
+     * @param eateries : List<Eatery> - The list of eateries
+     */
+    public void setEateriesFull(List<Eatery> eateries) {
+        eateriesFull.addAll(eateries);
+    }
+
+    @Override
+    public Filter getFilter() {
+        return eateryFilter;
+    }
+
+    /**
+     * Custom Eatery Filter
+     */
+    private Filter eateryFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<Eatery> eateriesTemp = new ArrayList<>();
+            FilterResults results = new FilterResults();
+
+            if (charSequence.toString().isEmpty()) {
+                results.values = eateriesFull;
+                results.count = eateriesFull.size();
+            } else {
+                String searchString = charSequence.toString().toLowerCase().trim();
+
+                for (Eatery eatery : eateriesFull) {
+                    if (eatery.getName().toLowerCase().contains(searchString)) {
+                        eateriesTemp.add(eatery);
+                    }
+                }
+
+                results.values = eateriesTemp;
+                results.count = eateriesTemp.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            if (filterResults.count > 0) {
+                setEateries((List<Eatery>) filterResults.values);
+            } else {
+                List<Eatery> empty = new ArrayList<>();
+                setEateries(empty);
+            }
+        }
+    };
 
     /**
      * ViewHolder Class
