df3c15c66796d5ee03ed9bf75457dab1eedf25be
Jonathan Winton <jw22gh@student.lnu.se>
2020-05-22 14:48:29 +0200

    Changing orderclass and creating enum classes

--- EasyFood/app/src/main/java/com/example/easyfood/model/Order.java
+++ EasyFood/app/src/main/java/com/example/easyfood/model/Order.java
@@ -1,36 +1,31 @@
 package com.example.easyfood.model;
 
-import java.util.List;
+import java.util.ArrayList;
+
 
 /**
- * <h1>Order Class</h1>
  * Holds the information about an order that has been placed.
  */
 public class Order {
-    private Object customer;
-    private int orderId;
-    private int orderNr;
-    private List<Object> products;
-    private Object payment;
 
-    public Order(Object customer, int orderId, int orderNr, Object payment, List<Object> products) {
-        this.customer = customer;
-        this.orderId = orderId;
-        this.orderNr = orderNr;
-        this.products = products;
-        this.payment = payment;
+    private  String OrderID;
+    private int OrderNumber;
+    private ArrayList<Product> products;
+    private String message;
+    private OrderPaymentMethodEnums paymentMethod;
+    private boolean isPaid;
+    private OrderStatusEnums orderStatus;
+    private double sum;
+    private String customerID;
+    private String restaurantID;
+
+    public Order (String restaurantID) {
+        this.restaurantID = restaurantID;
     }
 
-    /**
-     * Returns a string of all the order details.
-     * @return String
-     */
-    public String getOrder() {
-        return "Customer " + this.customer.toString() +
-                " Order ID: " + this.orderId +
-                " Order Number: " + this.orderNr +
-                " Payment: " + this.payment.toString() +
-                " Products:" + this.products.toString();
+    protected String getRestaurantID() {
+        return restaurantID;
     }
-    
+
+    //More getters and setters will be added here.
 }
