a51ec3f2bcd54e8698666556b676d2fc5768dec7
Olof Haga <oh222fv@student.lnu.se>
2020-05-23 12:32:27 +0200

    Adding getSpecificEatery(managerId)

--- EasyFood/app/src/main/java/com/example/easyfood/repository/EateryRepository.java
+++ EasyFood/app/src/main/java/com/example/easyfood/repository/EateryRepository.java
@@ -2,25 +2,33 @@ package com.example.easyfood.repository;
 
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.easyfood.db.Firebase;
 import com.example.easyfood.db.IEateriesCallback;
-import com.example.easyfood.db.IProductsCallback;
 import com.example.easyfood.model.Eatery;
-import com.example.easyfood.model.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * EateryRepository - Singleton
  */
 public class EateryRepository {
+    private String TAG = "EateryRepository";
     private static EateryRepository instance;
     private static Firebase fb;
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
     private ArrayList<Eatery> eateryList;
     private MutableLiveData<List<Eatery>> eateries = new MutableLiveData<>();
+    private MutableLiveData<Eatery> eatery = new MutableLiveData<>();
 
     /**
      * Returns an instance of the EateryRepository
@@ -54,5 +62,61 @@ public class EateryRepository {
 
         eateries.setValue(eateryList);
         return eateries;
+    }
+
+
+    /**
+     * Returns eatery by manager id
+     *
+     * @param managerId : String - The id of the manager
+     * @return eatery : LiveData<Eatery> - The eatery
+     */
+    public MutableLiveData<Eatery> getSpecificEatery(String managerId) {
+        eatery = new MutableLiveData<>();
+
+        getEateryFromDatabase(managerId, new IEateryCallback() {
+            @Override
+            public void send(Eatery newEatery) {
+                eatery.setValue(newEatery);
+            }
+        });
+
+        return eatery;
+    }
+
+    /**
+     * Gets a specific eatery from the database by its manager id
+     * @param managerId : String - Id of the manager
+     * @param callback : IEateryCallback - Callback
+     */
+    private void getEateryFromDatabase(String managerId, final IEateryCallback callback) {
+        database.collection("eateries").whereEqualTo("managerId", managerId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Eatery eatery = new Eatery();
+
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                eatery.setId(document.getString("id"));
+                                eatery.setManagerId(document.getString("managerId"));
+                                eatery.setName(document.getString("name"));
+                            }
+
+                            callback.send(eatery);
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Interface
+     */
+    private interface IEateryCallback {
+        void send(Eatery eatery);
     }
 }
