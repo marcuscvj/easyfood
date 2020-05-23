a0c77cae7a0c72d87960ce3ab39bb37f69c21ae2
Olof Haga <oh222fv@student.lnu.se>
2020-05-23 13:03:20 +0200

    Creating MainViewModel

--- EasyFood/app/src/main/java/com/example/easyfood/viewmodel/MainViewModel.java
+++ EasyFood/app/src/main/java/com/example/easyfood/viewmodel/MainViewModel.java
@@ -4,19 +4,19 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.easyfood.model.Eatery;
-import com.example.easyfood.repository.EateryRepository;
+import com.example.easyfood.model.User;
+import com.example.easyfood.repository.UserRepository;
 
 public class MainViewModel extends ViewModel {
-    private EateryRepository eateryRepository;
-    private MutableLiveData<Eatery> eatery;
+    private UserRepository userRepository;
+    private MutableLiveData<User> user;
 
     public void init() {
-        eateryRepository = EateryRepository.getInstance();
+        userRepository = UserRepository.getInstance();
     }
 
-    public LiveData<Eatery> getEateryId(String managerId) {
-        eatery = eateryRepository.getSpecificEatery(managerId);
-        return eatery;
+    public LiveData<User> getUser(String userId) {
+        user = userRepository.getCurrentUser(userId);
+        return user;
     }
 }
