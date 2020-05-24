ed1e347fb2fe85aa00869f42816ff50502c11bf9
Olof Haga <oh222fv@student.lnu.se>
2020-05-24 11:53:23 +0200

    Creating OrderViewModel

---
+++ EasyFood/app/src/main/java/com/example/easyfood/viewmodel/OrderViewModel.java
@@ -0,0 +1,31 @@
+package com.example.easyfood.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.easyfood.model.Order;
+import com.example.easyfood.repository.OrderRepository;
+
+public class OrderViewModel extends ViewModel {
+    private MutableLiveData<Order> order;
+    private OrderRepository orderRepository;
+
+    public void init(String orderId) {
+        if (order != null) {
+            return;
+        }
+
+        orderRepository = OrderRepository.getInstance();
+        order = orderRepository.getOrder(orderId);
+    }
+    /**
+     * Returns an order
+     *
+     * @return orders - LiveData<List<Order>> - The orders
+     */
+    public LiveData<Order> getOrder() {
+        return order;
+    }
+
+}
