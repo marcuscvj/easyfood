8173523b0d72a514a93ec77c65c4afbad6d78d72
Olof Haga <oh222fv@student.lnu.se>
2020-05-25 14:17:09 +0200

    Removing Database package

--- EasyFood/app/src/main/java/com/example/easyfood/db/IEateriesCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.easyfood.db;
-
-import com.example.easyfood.model.Eatery;
-
-import java.util.ArrayList;
-
-public interface IEateriesCallback {
-    void send(ArrayList<Eatery> list);
-}
