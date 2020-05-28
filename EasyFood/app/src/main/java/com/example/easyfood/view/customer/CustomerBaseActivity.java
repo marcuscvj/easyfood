bc810f53c65922bb0e4bd641508b7d84fbd7d4bd
Marcus Cvjeticanin <marcus.cvjeticanin1@gmail.com>
2020-05-28 13:37:43 +0200

    Not working code

--- EasyFood/app/src/main/java/com/example/easyfood/view/customer/CustomerBaseActivity.java
+++ EasyFood/app/src/main/java/com/example/easyfood/view/customer/CustomerBaseActivity.java
@@ -1,12 +1,17 @@
 package com.example.easyfood.view.customer;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -30,6 +35,7 @@ public class CustomerBaseActivity extends BaseActivity {
         viewModel.init(customerId);
 
         // Listener method call here
+        createNotificationChannel();
         onOrderChangeListener();
     }
 
@@ -64,9 +70,36 @@ public class CustomerBaseActivity extends BaseActivity {
             public void onChanged(String order) {
                 // Temporary
                 // send push notification here
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notifications_orderstatus")
+                        .setSmallIcon(R.drawable.ic_cart)
+                        .setContentTitle("Order Status Changed")
+                        .setContentText(order)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                // Issue the notification.
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+                notificationManager.notify("notificationId", "newMessageNotification");
+
                 System.out.println(order);
             }
         });
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("notifications_orderstatus", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
